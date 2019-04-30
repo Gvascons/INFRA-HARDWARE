b8d398d07bd598505fd79509c2e343de0ac746cc
João Gabriel de Araújo Vasconcelos <39220533+Gvascons@users.noreply.github.com>
2019-04-29 23:38:12 -0300

    Add files via upload

--- Projeto29_04_DESENV/Controle.sv
+++ Projeto29_04_DESENV/Controle.sv
@@ -3,6 +3,7 @@ module Controle(
     	input logic rst,
     	input logic [31:0] INSTR,
 	input logic AluZero,
+	input logic AluMenor,
     	input logic AluIgual,
 	output logic [1:0]Shift,
 	output logic [5:0]Num,
@@ -14,40 +15,67 @@ module Controle(
     	output logic loadRegB,
     	output logic loadRegAluOut,
     	output logic SelMux2,
+	output logic SelMuxAlu,
     	output logic [2:0]SelMuxMem,
     	output logic RegWrite,
     	output logic PCWriteCond,
     	output logic loadRegMemData,
     	output logic MemData_Read,
     	output logic SelMuxPC,
-    	output logic [1:0]SelMux4);
+    	output logic [2:0]SelMux8);
  	
     	parameter SUM = 3'b001; //1
     	parameter SUB = 3'b010; //2
     	parameter LOAD = 3'b000; //0
+	parameter AND = 3'b011; //3
  	
     	//ESTADOS DA MÁQUINA DE ESTADOS
     	parameter RESET = 20;
     	parameter FETCH = 21; 
     	parameter DECODE = 22; 
-    	parameter sumOP1 = 4;
-    	parameter sumOP2 = 5;  
-    	parameter subOP1 = 6;
-    	parameter subOP2 = 7; 
+    	parameter sumOP = 4;
+    	//parameter sumOP2 = 5;
+    	parameter subOP = 6;
+    	//parameter subOP2 = 7; 
     	parameter beqOP1 = 8;
     	parameter bneOP1 = 10; 
-    	parameter sumsubWrite1 = 11;
-    	parameter sumsubWrite2 = 12;
-    	parameter memDRead1 = 13;
-    	parameter memDRead2 = 14;
+    	parameter addiOP1 = 11;
+    	parameter addiOP2 = 12;
+    	parameter ldOP1 = 13;
+    	parameter ldOP2 = 14;
 	parameter loadWrite = 15;
-    	parameter memDWrite1 = 16;
-    	parameter memDWrite2 = 17;
+    	parameter sdOP1 = 16;
+    	parameter sdOP2 = 17;
     	parameter luiOP1 = 18;
 	parameter luiOP2 = 19;
+	parameter andOP = 23;
+	parameter sltOP = 24;
+	parameter sltiOP = 25;
+	parameter wreg_op = 26;
+	parameter jalrOP1 = 27;
+	parameter jalrOP2 = 28;
+	parameter jalrOP3 = 29; 
+	parameter jalrOP4 = 48;
+	parameter srliOP1 = 30;
+	parameter sraiOP1 = 31;
+	parameter slliOP1 = 32;
+	parameter lbOP1 = 33;
+ 	parameter lhOP1 = 34;
+	parameter lwOP1 = 35;
+	parameter lbuOP1 = 37;
+	parameter lhuOP1 = 38;
+	parameter lwuOP1 = 39;
+	parameter nopOP1 = 40;
+	parameter breakOP1 = 41;
+	parameter swOP1 = 42;
+	parameter shOP1 = 43;
+	parameter sbOP1 = 44;
+	parameter bgeOP1 = 45;
+	parameter bltOP1 = 46;
+	parameter jalOP1 = 47;
  
-    	reg [5:0]state;
-    	reg [5:0]nextState;
+    	reg [9:0]state;
+    	reg [9:0]nextState;
  
 	always_ff @(posedge rst or posedge clk) begin
         	if (rst) begin
@@ -64,7 +92,8 @@ module Controle(
 				Shift = 0;
 				SelMuxPC = 0;
 				SelMux2 = 0;
-    	        		SelMux4 = 0;
+    	        		SelMux8 = 0;
+				SelMuxAlu = 0;
     	       			SelMuxMem = 0;
     	       			PCwrite = 0;
 				PCWriteCond = 0;
@@ -84,7 +113,8 @@ module Controle(
 				Shift = 0;
 				SelMuxPC = 0;
 				SelMux2 = 0;
-			    	SelMux4 = 1;
+			    	SelMux8 = 1;
+				SelMuxAlu = 0;
 			       	SelMuxMem = 1;
 			    	PCwrite = 1;
 				PCWriteCond = 0;
@@ -104,7 +134,8 @@ module Controle(
 				Shift = 0;
 				SelMuxPC = 0;
 				SelMux2 = 0;
-			    	SelMux4 = 3;
+			    	SelMux8 = 3;
+				SelMuxAlu = 0;
 			       	SelMuxMem = 0;
 			    	PCwrite = 0;
 				PCWriteCond = 0;
@@ -125,67 +156,66 @@ module Controle(
 
 					7'b0110011: begin   // Tipo R
                                 		if (INSTR[31:25] == 7'b0000000) begin
-							if (INSTR [14:12] = 3'b000) begin // add
-							nextState = sumOP1;
-						   end
-							if (INSTR [14:12] = 3'b111) begin // and
-							nextState = andOP1;
-						   end
-							if (INSTR [14:12] = 3'b010) begin // slt
-							nextState = sltOP1;
-						   end
+							if (INSTR [14:12] == 3'b000) begin // add
+								nextState = sumOP;
+						   	end
+							if (INSTR [14:12] == 3'b111) begin // and
+								nextState = andOP;
+						   	end
+							if (INSTR [14:12] == 3'b010) begin // slt
+								nextState = sltOP;
+						   	end
                                 		end
                         	        	if (INSTR[31:25] == 7'b0100000) begin // sub
-                        	        		nextState = subOP1;
+                        	        		nextState = subOP;
                         	        	end
 					end
   
 	
 					7'b0010011: begin   // Tipo I 
-						if (INSTR [14:12] = 3'b000) begin // addi
-						nextState = sumsubWrite1;
-					   end
-						if (INSTR [14:12] = 3'b010) begin // slti
-						nextState = sltiOP1;
-					   end
-						if (INSTR [14:12] = 3'b101) begin 
-						   if (INSTR [31:26] = 6'b000000) // srli
-						 nextState = srliOP1;
-						end
-						   if (INSTR [31:26] = 6'b010000) // srai
-						 nextState = sraiOP1;
-						end
-					   end
-						if (INSTR [14:12] = 3'b001) begin // slli
-						nextState = slliOP1;
-					   end
-
+						if (INSTR [14:12] == 3'b000) begin // addi
+							nextState = addiOP1;
+					   	end
+						if (INSTR [14:12] == 3'b010) begin // slti
+							nextState = sltiOP;
+					   	end
+						if (INSTR [14:12] == 3'b101) begin 
+						   	if (INSTR [31:26] == 6'b000000) begin// srli
+						 		nextState = srliOP1;
+							end
+						   	if (INSTR [31:26] == 6'b010000) begin // srai
+						 		nextState = sraiOP1;
+							end
+					   	end
+						if (INSTR [14:12] == 3'b001) begin // slli
+							nextState = slliOP1;
+					   	end
                         	    	end
 					
 					//jalr junto com o bne, blt e bge
 					
 					7'b0000011: begin  
-						if (INSTR [14:12] = 3'b000) begin //lb
-						nextState = lbOP1;
-					    end
-						if (INSTR [14:12] = 3'b001) begin //lh
-						nextState = lhOP1;
-					    end
-						if (INSTR [14:12] = 3'b010) begin //lw
-						nextState = lwOP1;
-					    end
-						if (INSTR [14:12] = 3'b011) begin //ld
-						nextState = memDRead1;
-					    end	
-						if (INSTR [14:12] = 3'b100) begin //lbu
-						nextState = lbuOP1;
-					    end
-						if (INSTR [14:12] = 3'b101) begin //lhu
-						nextState = lhuOP1;
-					    end
-						if (INSTR [14:12] = 3'b110) begin //lwu
-						nextState = lwuOP1;
-					    end
+						if (INSTR [14:12] == 3'b000) begin //lb
+							nextState = lbOP1;
+					    	end
+						if (INSTR [14:12] == 3'b001) begin //lh
+							nextState = lhOP1;
+					    	end
+						if (INSTR [14:12] == 3'b010) begin //lw
+							nextState = lwOP1;
+					    	end
+						if (INSTR [14:12] == 3'b011) begin //ld
+							nextState = ldOP1;
+					    	end	
+						if (INSTR [14:12] == 3'b100) begin //lbu
+							nextState = lbuOP1;
+					    	end
+						if (INSTR [14:12] == 3'b101) begin //lhu
+							nextState = lhuOP1;
+					    	end
+						if (INSTR [14:12] == 3'b110) begin //lwu
+							nextState = lwuOP1;
+					    	end
 					end  
 					
 					7'b0010011: begin  // nop
@@ -198,18 +228,18 @@ module Controle(
 					
 					
 					7'b0100011: begin   // Tipo S (store)
-						if (INSTR [14:12] = 3'b111) begin //sd
-						nextState = memDWrite1; 
-                        	    	   end	
-						if (INSTR [14:12] = 3'b010) begin //sw
-						nextState = swOP1; 
-                        	    	   end	
-						if (INSTR [14:12] = 3'b001) begin //sh
-						nextState = shOP1; 
-                        	    	   end
-						if (INSTR [14:12] = 3'b000) begin //sb
-						nextState = sbOP1; 
-                        	    	   end
+						if (INSTR [14:12] == 3'b111) begin //sd
+							nextState = sdOP1; 
+                        	    	   	end	
+						if (INSTR [14:12] == 3'b010) begin //sw
+							nextState = swOP1; 
+                        	    	   	end	
+						if (INSTR [14:12] == 3'b001) begin //sh
+							nextState = shOP1; 
+                        	    	   	end
+						if (INSTR [14:12] == 3'b000) begin //sb
+							nextState = sbOP1; 
+                        	    	   	end
 					end
 
 					7'b1100011: begin   // Tipo SB (beq)
@@ -217,18 +247,18 @@ module Controle(
                         	    	end
 	
 					7'b1100111: begin   // Tipo SB + jalr
-						if (INSTR [14:12] = 3'b000) begin //jalr
-						nextState = jalrOP1;
-					     end
-						if (INSTR [14:12] = 3'b001) begin //bne
-						nextState = bneOP1;
-					     end
-						if (INSTR [14:12] = 3'b101) begin  //bge
-						nextState = bgeOP1;
-					     end
-						if (INSTR [14:12] = 3'b100) begin  //blt
-						nextState = bltOP1;
-					     end
+						if (INSTR [14:12] == 3'b000) begin //jalr
+							nextState = jalrOP1;
+					     	end
+						if (INSTR [14:12] == 3'b001) begin //bne
+							nextState = bneOP1;
+					     	end
+						if (INSTR [14:12] == 3'b101) begin  //bge
+							nextState = bgeOP1;
+					    	end
+						if (INSTR [14:12] == 3'b100) begin  //blt
+							nextState = bltOP1;
+					     	end
 
                         	        end
 
@@ -239,14 +269,14 @@ module Controle(
 					7'b1101111: begin // Tipo UJ (jal)
 						nextState = jalOP1;
 					end
-				endcase
-			end
+				endcase //case(INSTR[6:0])
+			end //DECODE
 
-			sumOP1: begin
+			sumOP: begin
 				Shift = 0;
 				SelMuxPC = 0;
 				SelMux2 = 1;
-			    	SelMux4 = 0;
+			    	SelMux8 = 0;
 			       	SelMuxMem = 0;
 			    	PCwrite = 0;
 				PCWriteCond = 0;
@@ -259,34 +289,35 @@ module Controle(
 				loadRegMemData = 0;
 				MemData_Read = 0;
 			    	AluOperation = SUM;
-				nextState = sumOP2;
+				nextState = wreg_op;
 			end
                                         
-			sumOP2: begin
+
+			andOP: begin
 				Shift = 0;
 				SelMuxPC = 0;
-				//SelMux2 = 0;
-			    	//SelMux4 = 1;
+				SelMux2 = 1;
+			    	SelMux8 = 0;
 			       	SelMuxMem = 0;
 			    	PCwrite = 0;
 				PCWriteCond = 0;
-				RegWrite = 1;
+				RegWrite = 0;
 			    	MemRead = 0;
 			    	LoadIR = 0;
 			    	loadRegA = 0;
 			    	loadRegB = 0;
-				loadRegAluOut = 0;
+				loadRegAluOut = 1;
 				loadRegMemData = 0;
 				MemData_Read = 0;
-			    	//AluOperation = 0;
-				nextState = RESET;
+			    	AluOperation = AND;
+				nextState = wreg_op;
 			end
-			
-			subOP1: begin
+                                        
+			subOP: begin
 				Shift = 0;
 				SelMuxPC = 0;
 				SelMux2 = 1;
-			    	SelMux4 = 0;
+			    	SelMux8 = 0;
 			       	SelMuxMem = 0;
 			    	PCwrite = 0;
 				PCWriteCond = 0;
@@ -299,14 +330,50 @@ module Controle(
 				loadRegMemData = 0;
 				MemData_Read = 0;
 			    	AluOperation = SUB;
-				nextState = subOP2;
+				nextState = wreg_op;
 			end
 
-			subOP2: begin
+			sltOP: begin
+				Shift = 0;
+				SelMux2 = 1;
+			    	SelMux8 = 0;
+				SelMuxAlu = 1;
+			       	SelMuxMem = 0;
+				RegWrite = 0;
+			    	MemRead = 0;
+				MemData_Read = 0;
+			    	LoadIR = 0;
+			    	loadRegA = 0;
+			    	loadRegB = 0;
+				loadRegAluOut = 0;
+				loadRegMemData = 0;
+			    	AluOperation = LOAD;
+				nextState = wreg_op;
+			end
+
+			sltiOP: begin
+				Shift = 0;
+				SelMux2 = 1;
+			    	SelMux8 = 2;
+				SelMuxAlu = 1;
+			       	SelMuxMem = 0;
+				RegWrite = 0;
+			    	MemRead = 0;
+				MemData_Read = 0;
+			    	LoadIR = 0;
+			    	loadRegA = 0;
+			    	loadRegB = 0;
+				loadRegAluOut = 0;
+				loadRegMemData = 0;
+			    	AluOperation = LOAD;
+				nextState = wreg_op;
+			end
+
+			wreg_op: begin
 				Shift = 0;
 				SelMuxPC = 0;
 				//SelMux2 = 0;
-			    	//SelMux4 = 1;
+			    	//SelMux8 = 1;
 			       	SelMuxMem = 0;
 			    	PCwrite = 0;
 				PCWriteCond = 0;
@@ -322,10 +389,90 @@ module Controle(
 				nextState = RESET;
 			end
 
+			jalrOP1: begin 
+				Shift = 0;
+				SelMuxPC = 1;
+				SelMux2 = 0;
+			    	SelMux8 = 4;
+			       	//SelMuxMem = 0;
+			    	PCwrite = 0;
+				PCWriteCond = 0;
+				RegWrite = 0;
+			    	MemRead = 0;
+			    	LoadIR = 0;
+			    	loadRegA = 0;
+			    	loadRegB = 0;
+				loadRegAluOut = 0;
+				loadRegMemData = 0;
+				MemData_Read = 0;
+			    	AluOperation = SUM;
+				nextState = jalrOP2;
+			end
+
+			jalrOP2: begin
+				Shift = 0;
+				SelMuxPC = 0;
+				//SelMux2 = 1;
+			    	//SelMux8 = 2;
+			       	SelMuxMem = 0;
+			    	PCwrite = 0;
+				PCWriteCond = 0;
+				RegWrite = 1;
+			    	MemRead = 0;
+			    	LoadIR = 0;
+			    	loadRegA = 1;
+			    	loadRegB = 0;
+				loadRegAluOut = 0;
+				loadRegMemData = 0;
+				MemData_Read = 0;
+			    	//AluOperation = 0;
+				nextState = jalrOP3;
+			end
+
+			jalrOP3: begin
+				Shift = 0;
+				SelMuxPC = 0;
+				SelMux2 = 1;
+			    	SelMux8 = 3;
+			       	SelMuxMem = 0;
+			    	PCwrite = 0;
+				PCWriteCond = 0;
+				RegWrite = 0;
+			    	MemRead = 0;
+			    	LoadIR = 0;
+			    	loadRegA = 0;
+			    	loadRegB = 0;
+				loadRegAluOut = 1;
+				loadRegMemData = 0;
+				MemData_Read = 0;
+			    	AluOperation = SUM;
+				nextState = jalrOP4;
+			end
+
+			jalrOP4: begin 
+				Shift = 0;
+				SelMuxPC = 0;
+				SelMux2 = 0;
+			    	SelMux8 = 5;
+			       	SelMuxMem = 0;
+			    	PCwrite = 1;
+				PCWriteCond = 0;
+				RegWrite = 0;
+			    	MemRead = 0;
+			    	LoadIR = 0;
+			    	loadRegA = 0;
+			    	loadRegB = 0;
+				loadRegAluOut = 0;
+				loadRegMemData = 0;
+				MemData_Read = 0;
+			    	//AluOperation = 0;
+				nextState = RESET;
+			end
+			
 			beqOP1: begin
 				Shift = 0;
 				SelMux2 = 1;
-			    	SelMux4 = 0;
+			    	SelMux8 = 0;
 			       	SelMuxMem = 1;
 				RegWrite = 0;
 			    	MemRead = 0;
@@ -353,7 +500,7 @@ module Controle(
 			bneOP1: begin
 				Shift = 0;
 				SelMux2 = 1;
-			    	SelMux4 = 0;
+			    	SelMux8 = 0;
 			       	SelMuxMem = 1;
 				RegWrite = 0;
 			    	MemRead = 0;
@@ -378,11 +525,11 @@ module Controle(
 				nextState = RESET;
 			end
 
-			sumsubWrite1: begin
+			addiOP1: begin
 				Shift = 0;
 				SelMuxPC = 0;
 				SelMux2 = 1;
-    	        		SelMux4 = 2;
+    	        		SelMux8 = 2;
     	       			SelMuxMem = 0;
     	       			PCwrite = 0;
 				PCWriteCond = 0;
@@ -395,14 +542,14 @@ module Controle(
 				loadRegMemData = 0;
 				MemData_Read = 0;
 				loadRegAluOut = 1;
-    	        		nextState = sumsubWrite2;
+    	        		nextState = addiOP2;
 			end
 
-			sumsubWrite2: begin
+			addiOP2: begin
 				Shift = 0;
 				SelMuxPC = 0;
 				SelMux2 = 1;
-			    	SelMux4 = 2;
+			    	SelMux8 = 2;
 			       	SelMuxMem = 0;
 			    	PCwrite = 0;
 				PCWriteCond = 0;
@@ -418,12 +565,12 @@ module Controle(
 				nextState = RESET;
                  	end
 			
-			memDRead1: begin
+			ldOP1: begin
 				Shift = 0;
 				SelMuxPC = 0;
 				AluOperation = SUM;
 				SelMux2 = 1;
-			    	SelMux4 = 2;
+			    	SelMux8 = 2;
 			       	SelMuxMem = 1;
 			    	PCwrite = 0;
 				PCWriteCond = 0;
@@ -435,14 +582,14 @@ module Controle(
 			    	//loadRegB = 0;
 				loadRegAluOut = 1;
 				loadRegMemData = 0;
-				nextState = memDRead2;
+				nextState = ldOP2;
 			end
 
-			memDRead2: begin
+			ldOP2: begin
 				Shift = 0;
 				SelMuxPC = 0;
 				//SelMux2 = 0;
-			    	//SelMux4 = 0;
+			    	//SelMux8 = 0;
 			       	SelMuxMem = 0;
 			    	PCwrite = 0;
 				PCWriteCond = 0;
@@ -462,7 +609,7 @@ module Controle(
 				Shift = 0;
 				SelMuxPC = 0;
 				//SelMux2 = 0;
-			    	//SelMux4 = 0;
+			    	//SelMux8 = 0;
 			       	SelMuxMem = 1;
 			    	PCwrite = 0;
 				PCWriteCond = 0;
@@ -478,12 +625,12 @@ module Controle(
 				nextState = RESET;
 			end
 
-			memDWrite1: begin
+			sdOP1: begin
 				Shift = 0;
 				SelMuxPC = 0;
 				AluOperation = SUM;
 				SelMux2 = 1;
-			    	SelMux4 = 2;
+			    	SelMux8 = 2;
 			       	SelMuxMem = 1;
 			    	PCwrite = 0;
 				PCWriteCond = 0;
@@ -495,14 +642,14 @@ module Controle(
 			    	//loadRegB = 0;
 				loadRegAluOut = 1;
 				loadRegMemData = 0;
-				nextState = memDWrite2;
+				nextState = sdOP2;
 			end
 
-			memDWrite2: begin
+			sdOP2: begin
 				Shift = 0;
 				SelMuxPC = 0;
 				//SelMux2 = 0;
-			    	//SelMux4 = 0;
+			    	//SelMux8 = 0;
 			       	SelMuxMem = 0;
 			    	PCwrite = 0;
 				PCWriteCond = 0;
@@ -522,7 +669,7 @@ module Controle(
 				Shift = 0;
 				SelMuxPC = 0;
 				//SelMux2 = 0;
-			    	//SelMux4 = 2;
+			    	//SelMux8 = 2;
 			       	//SelMuxMem = 2;
 			    	PCwrite = 0;
 				PCWriteCond = 0;
@@ -542,7 +689,7 @@ module Controle(
 				Shift = 0;
 				SelMuxPC = 0;
 				SelMux2 = 0;
-			    	SelMux4 = 2;
+			    	SelMux8 = 2;
 			       	SelMuxMem = 2;
 			    	PCwrite = 0;
 				PCWriteCond = 0;
@@ -557,6 +704,6 @@ module Controle(
 			    	AluOperation = 0;
 				nextState = RESET;
 			end 
-            	endcase
+            	endcase //case(state)
         end
 endmodule
